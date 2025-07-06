d6a3bdd1e7853c8fa74b684cfea345d4bd3f5aaf
santa9814 <mateusz.santarius@gmail.com>
2025-07-06 14:07:24 +0200

    MAJOR: zmieniono strukturę wywoływania menu (menu → uruchom_menu)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 # --- HEADER A ---
  feature/header-design-b
 print('Witaj świecie')
-def menu():
-    print('--- MENU ---')
-    print('1. Start')
-    print('2. O nas')
-    print('3. Kontakt')
-    print('4. Kalkulator')
+def uruchom_menu():
+    print("=== NOWE MENU ===")
+    print("1. Start")
+    print("2. O nas")
+    print("3. Kontakt")
+    print("4. Kalkulator")
 
 if __name__ == '__main__':
-    menu()
+    uruchom_menu()
